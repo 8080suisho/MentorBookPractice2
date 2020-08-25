bb8ff4b8cd1ca95e15fda3d03b440158df9694a1
Mizuki Morohoshi <8080suisho@gmail.com>
2020-08-26 00:55:44 +0900

    MentorBook二回目の練習

--- MentorBookPractice2/ViewController.swift
+++ MentorBookPractice2/ViewController.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
+    
+    var index:Int = 0
+    
+    @IBOutlet var imageView:UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mentorArray.append(Mentor(name: "ながた", course: "iPhone", imageName: "nagata.jpg"))
+        mentorArray.append(Mentor(name: "りょう", course: "Unity", imageName:"ryo.jpg"))
+        mentorArray.append(Mentor(name: "たいてぃ", course: "WebS,WebD", imageName: "taithi.jpg"))
+        
+        setUI()
+    }
+    
+    func setUI(){
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
+    }
+    
+    
+    
+    
+    @IBAction func mae(){
+        index = index - 1
+        if index < 0{
+            index = 2
+        }
+        setUI()
+    }
+    
+    @IBAction func tugi(){
+        index = index + 1
+        if index > 2{
+            index = 0
+        }
+        setUI()
     }
 
 
